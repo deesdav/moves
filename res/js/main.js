fd46eb0496599840a0677eb53c4fb84fa84e8201
deesdav <105449474+deesdav@users.noreply.github.com>
2022-11-08 20:16:21 +0100

    update

--- res/js/main.js
+++ res/js/main.js
@@ -4,6 +4,33 @@ const rightBtn = document.getElementById("rightBtn");
 const bottomBtn = document.getElementById("bottomBtn");
 const player = document.getElementById("player");
 
+const randomNumber = (maximum, minimum) => {
+    const myNum = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+    return myNum;
+    
+}
+
+console.log(randomNumber(0, 255));
+
+const changeColor = () => {
+    let r = randomNumber(0,255);
+    let g = randomNumber(0,255);
+    let b = randomNumber(0,255);
+    player.style.backgroundColor = `rgb(${r},${g},${b})`;
+}
+const myInterval = setInterval(() => {
+    changeColor()
+},1000);
+
+player.onclick = () => {
+    changeColor();
+    clearInterval(myInterval);
+}
+
+
+
+
+
 let x = 200;
 let y = 200;
 
